ec8bda3fae0ec1b08e3573893b04297904fff5d6
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2022-11-10 19:19:40 +0000

    create transport once and reuse it

--- lib/http/reverse_proxy.go
+++ lib/http/reverse_proxy.go
@@ -10,29 +10,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var transport *http.Transport
+
+func init() {
+	transport = &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		TLSHandshakeTimeout:   3 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Minute,
+		IdleConnTimeout:       10 * time.Second,
+		DisableKeepAlives:     false,
+		MaxIdleConns:          0,
+		MaxIdleConnsPerHost:   0,
+		MaxConnsPerHost:       0,
+	}
+}
+
+func requestDirector(req *http.Request, targetServer string, host string) func(req *http.Request) {
+	return func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = targetServer
+
+		req.Header.Set("Host", host)
+	}
+}
+
 func ReverseProxy(targetServer string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		host := c.Request.Host
 
-		director := func(req *http.Request) {
-			req.URL.Scheme = "http"
-			req.URL.Host = targetServer
-
-			req.Header.Set("Host", host)
-		}
-
-		transport := &http.Transport{
-			Proxy:                 http.ProxyFromEnvironment,
-			TLSHandshakeTimeout:   3 * time.Second,
-			MaxIdleConns:          1000,
-			MaxIdleConnsPerHost:   10,
-			MaxConnsPerHost:       0,
-			IdleConnTimeout:       10 * time.Second,
-			ResponseHeaderTimeout: 10 * time.Minute,
-		}
-
 		proxy := &httputil.ReverseProxy{
-			Director:  director,
+			Director:  requestDirector(c.Request, targetServer, host),
 			Transport: transport,
 			ErrorHandler: func(writer http.ResponseWriter, request *http.Request, err error) {
 				if !strings.Contains(err.Error(), "context canceled") {
